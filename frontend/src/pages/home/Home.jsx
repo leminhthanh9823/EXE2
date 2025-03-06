72fecf96fd006ee0728c28c1db31e04d972a51ef
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-03-06 11:09:21 +0700

    chang pic

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,10 +6,11 @@ import WorkArea from "../../components/section/home/WorkArea/WorkArea";
 
 export default function Home() {
   let slides = [
-    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
-    "https://wallpapercave.com/wp/wp3386769.jpg",
-    "https://wallpaperaccess.com/full/809523.jpg",
-    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
+
+    "https://file.hstatic.net/200000201805/article/a_9a1cb7cc4120473d99c61950a785ef43.jpg",
+    "https://file.hstatic.net/200000201805/article/1_ba82ef60041c4672b2463fe18c2e6f5f.jpg",
+    "https://heartfulflavours.com/cdn/shop/articles/Healthy_Dietary_Pattern.png?v=1694096108",
+    "https://file.hstatic.net/200000827051/article/thuc-don-giam-can-trong-7-ngay-2_d3f0b02957444ff3b6018d6c6314137e.jpg",
   ];
   return (
     <div className="flex flex-col min-h-screen">
