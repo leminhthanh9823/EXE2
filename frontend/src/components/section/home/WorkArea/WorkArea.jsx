feb7ee345a1e6f389b9e0a377dcb42395ba94fab
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-03-24 14:54:59 +0700

    update homepage

--- frontend/src/components/section/home/WorkArea/WorkArea.jsx
+++ frontend/src/components/section/home/WorkArea/WorkArea.jsx
@@ -35,118 +35,130 @@ export default function WorkArea() {
     ];
     return (
         <section
-            className="work pt-5 pb-32 bg-cover bg-center"
+            className="work pt-5 pb-32 bg-cover bg-center relative"
             style={{
                 backgroundImage: 'url("public/assets/images/banner1.jpg")',
             }}
         >
-            <div className="container mx-auto">
-                {/* first section */}
-                <div className="pb-16 border-b">
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center">
-                        <div>
-                            <div className="section-header m-0">
-                                <h5
-                                    className="wow fadeInUp text-lg font-semibold flex items-center"
-                                    data-wow-duration="1.2s"
-                                    data-wow-delay=".2s"
-                                    style={{
-                                        visibility: "visible",
-                                        animationDuration: "1.2s",
-                                        animationDelay: "0.2s",
-                                        animationName: "fadeInUp",
-                                    }}
-                                >
-                                    <img
-                                        src="https://i.postimg.cc/5tW5h4Nd/fitmenulogo.png"
-                                        alt="Leaf Icon"
-                                        className="mr-2"
-                                        style={{ width: "30%", height: "30%" }}
-                                    />
-                                    Sứ mệnh của chúng tôi
-                                </h5>
-                                <h2
-                                    className="wow fadeInUp text-3xl font-bold mt-4"
-                                    data-wow-duration="1.4s"
-                                    data-wow-delay=".4s"
-                                    style={{
-                                        visibility: "visible",
-                                        animationDuration: "1.4s",
-                                        animationDelay: "0.4s",
-                                        animationName: "fadeInUp",
-                                    }}
-                                >
-                                    Hàng đầu trong thiết kế thực đơn eatclean
-                                </h2>
-                                <p
-                                className="wow fadeInUp text-lg"
-                                data-wow-duration="1.6s"
-                                data-wow-delay=".6s"
+            <div className="absolute inset-0 "></div>
+            <div className="relative z-10">
+                {/* banner */}
+                <div className="bg-black opacity-70">
+                <img
+                    src="https://file.hstatic.net/200000201805/article/a_9a1cb7cc4120473d99c61950a785ef43.jpg"
+                    alt="banner"
+                    className="w-full object-cover"
+                    style={{ height: "70vh" }}
+                />
+                </div>
+                {/* mission section */}
+                                <div className="absolute inset-0 flex flex-col items-center justify-center">
+                                    <div className="container mx-auto">
+                                        <div>
+                                            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 items-center">
+                                                <div>
+                                                    <div className="section-header m-0 bg-gradient-to-r from-green-700 to-green-300" 
+                                                        style={{ padding:'20px', borderRadius:'10px', marginTop:'250px'}}>
+                                                        
+                                                        {/* <h5
+                                                            className="wow fadeInUp text-lg font-semibold flex items-center"
+                                                            data-wow-duration="1.2s"
+                                                            data-wow-delay=".2s"
+                                                            style={{
+                                                                visibility: "visible",
+                                                                animationDuration: "1.2s",
+                                                                animationDelay: "0.2s",
+                                                                animationName: "fadeInUp",
+                                                            }}
+                                                        >
+                                                            <img
+                                                                src="https://i.postimg.cc/5tW5h4Nd/fitmenulogo.png"
+                                                                alt="Leaf Icon"
+                                                                className="mr-2"
+                                                                style={{ width: "30%", height: "30%" }}
+                                                            />
+                                                            Sứ mệnh của chúng tôi
+                                                        </h5> */}
+                                        <h2
+                                            className="wow fadeInUp text-3xl font-bold mt-4 pb-4"
+                                            data-wow-duration="1.4s"
+                                            data-wow-delay=".4s"
+                                            style={{
+                                                visibility: "visible",
+                                                animationDuration: "1.4s",
+                                                animationDelay: "0.4s",
+                                                animationName: "fadeInUp",
+                                                
+                                            }}
+                                        >
+                                            <h2 style={{marginBottom:'3px', color:'white'}}>Hàng đầu trong thiết kế</h2>
+                                            <span style={{padding:'3px', backgroundColor:'#FFC000', color:'black', borderRadius:'5px'}}>Thực đơn Eatclean</span>
+                                        </h2>
+                                        <p
+                                            className="wow fadeInUp text-lg"
+                                            data-wow-duration="1.6s"
+                                            data-wow-delay=".6s"
+                                            style={{
+                                                visibility: "visible",
+                                                animationDuration: "1.6s",
+                                                animationDelay: "0.6s",
+                                                animationName: "fadeInUp",
+                                            }}
+                                        >
+                                        </p>
+
+                                        <Link to="/my-menu">
+                                            <button className="px-6 py-3 rounded-lg hover:bg-green-700" style={{backgroundColor:'white', color:'rgb(22 163 84)'}}>
+                                                <strong>Thử ngay</strong>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            {/* Nút "Thử ngay" */}
+                            <div className="flex justify-start mb-8 mt-4">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    {/* goal section */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mt-8" style={{padding:'0px 100px'}}>
+                        {sectionDescription.map((item, index) => (
+                            <div
+                                key={item.id}
+                                className="wow fadeInDown bg-white shadow-md rounded-lg relative"
+                                data-wow-duration={`${1.2 + index * 0.2}s`}
+                                data-wow-delay={`${0.2 + index * 0.2}s`}
                                 style={{
                                     visibility: "visible",
-                                    animationDuration: "1.6s",
-                                    animationDelay: "0.6s",
-                                    animationName: "fadeInUp",
+                                    animationDuration: `${1.2 + index * 0.2}s`,
+                                    animationDelay: `${0.2 + index * 0.2}s`,
+                                    animationName: "fadeInDown",
                                 }}
                             >
-                                Chúng tôi muốn trở thành nền tảng hàng đầu trong lĩnh vực tư vấn 
-                                và thiết kế thực đơn Eat Clean, góp phần thay đổi nhận thức cộng đồng 
-                                về dinh dưỡng, giúp khách hàng duy trì lối sống khỏe mạnh thông qua 
-                                chế độ ăn uống cân bằng và phù hợp với nhu cầu cá nhân.
-                            </p>
-                            </div>
-                        </div>
-                        <div>
-                            <img src="https://file.hstatic.net/200000201805/article/a_9a1cb7cc4120473d99c61950a785ef43.jpg" alt="" />
-                        </div>
-                    </div>
-                    {/* Nút "Thử ngay" */}
-                    <div className="flex justify-start mb-8 mt-4">
-                    <Link to="/my-menu">
-                        <button className="px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700">
-                        <strong>Thử ngay</strong>
-                        </button>
-                    </Link>
-                    </div>
-                </div>
-
-
-                {/* second section */}
-                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-                    {sectionDescription.map((item, index) => (
-                        <div
-                            key={item.id}
-                            className="wow fadeInDown bg-white shadow-md rounded-lg relative"
-                            data-wow-duration={`${1.2 + index * 0.2}s`}
-                            data-wow-delay={`${0.2 + index * 0.2}s`}
-                            style={{
-                                visibility: "visible",
-                                animationDuration: `${1.2 + index * 0.2}s`,
-                                animationDelay: `${0.2 + index * 0.2}s`,
-                                animationName: "fadeInDown",
-                            }}
-                        >
-                            <div className="work__item">
-                                <div className="work__item-icon">
-                                    <img src={item.icon} alt={`${item.title} Icon`} className="mr-2" />
-                                    <span className="text-lg font-semibold">{`0${item.id}`}</span>
-                                </div>
-                                <h3 >
-                                    <a href="service-single.html">{item.title}</a>
-                                </h3>
-                                <p >{item.description}</p>
-                                <a
-                                    className="work__item-arrow"
-                                    href="service-single.html"
-                                >
-                                    <i className="fa-solid fa-arrow-right"></i>
-                                </a>
-                                <div className="work__item-leaf ">
-                                    <img src="assets/images/work-leaf.png" alt={`${item.title} Leaf`} />
+                                <div className="work__item">
+                                    <div className="work__item-icon">
+                                        <img src={item.icon} alt={`${item.title} Icon`} className="mr-2" />
+                                        <span className="text-lg font-semibold">{`0${item.id}`}</span>
+                                    </div>
+                                    <h3 >
+                                        <a href="service-single.html">{item.title}</a>
+                                    </h3>
+                                    <p >{item.description}</p>
+                                    <a
+                                        className="work__item-arrow"
+                                        href="service-single.html"
+                                    >
+                                        <i className="fa-solid fa-arrow-right"></i>
+                                    </a>
+                                    <div className="work__item-leaf ">
+                                        <img src="assets/images/work-leaf.png" alt={`${item.title} Leaf`} />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </section>
